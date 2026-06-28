3cfd86b52862461c7c754a19fae98b49fadf5767
trivialloop <trivial.loop@gmail.com>
2026-06-28 21:14:04 +0200

    Release 1.12.1 (#127)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
     defaultConfig {
         minSdk = 24
         targetSdk = 37
-        versionCode = 17
-        versionName = "1.12.0"
+        versionCode = 18
+        versionName = "1.12.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
